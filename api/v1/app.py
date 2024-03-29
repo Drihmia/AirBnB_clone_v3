cfe35d403131f107d00bf233d6e54bfa42ac499e
ubonisrael <akpanudohubonisrael@gmail.com>
2024-03-29 15:27:51 +0100

    completed tasks 12

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """The main module"""
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from os import getenv
 from models import storage
 from api.v1.views import app_views
@@ -8,6 +9,7 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources="/*", origins="0.0.0.0")
 
 
 @app.teardown_appcontext
