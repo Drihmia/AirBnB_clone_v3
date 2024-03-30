5c34fe24c94cf31891791c8f9e9af6c2026394e3
Drihmia <drihmia.redouane@gmail.com>
2024-03-30 00:46:27 +0000

    implemented task 13 and 14

--- models/base_model.py
+++ models/base_model.py
@@ -66,6 +66,12 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+
+        # Task 14: Security improvements!
+        # +Remove password for DB storage mode.
+        if models.storage_t == 'db':
+            if 'password' in new_dict:
+                del new_dict['password']
         return new_dict
 
     def delete(self):
--- models/user.py
+++ models/user.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ holds class User"""
+import hashlib
+from sqlalchemy import event
 import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
@@ -18,10 +20,30 @@ class User(BaseModel, Base):
         reviews = relationship("Review", backref="user")
     else:
         email = ""
-        password = ""
+        _password = ""
         first_name = ""
         last_name = ""
+
+        @property
+        def password(self):
+            """getter for password"""
+            return self._password
+
+        @password.setter
+        def password(self, value):
+            """ Setter for password"""
+            self._password = hashlib.md5(value.encode()).hexdigest()
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+
+def hash_password_before_insert_or_update(_, __, target):
+    if target.password is not None:
+        # Hash the password using MD5
+        target.password = hashlib.md5(target.password.encode()).hexdigest()
+
+
+event.listen(User, 'before_insert', hash_password_before_insert_or_update)
+event.listen(User, 'before_update', hash_password_before_insert_or_update)
